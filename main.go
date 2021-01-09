dbff5025e93586b6cfeaccdc94767572ddf8c65c
Raymond Florendo <raymondflorendo.320@gmail.com>
2021-01-10 01:07:33 +0800

    Add database.

--- main.go
+++ main.go
@@ -15,23 +15,25 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api/v1/todo", todo.GetTodos)
 	app.Get("/api/v1/todo/:id", todo.GetTodo)
 	app.Post("/api/v1/todo", todo.NewTodo)
-	app.Delete("/api/v1/book/:id", todo.DeleteTodo)
+	app.Delete("/api/v1/todo/:id", todo.DeleteTodo)
 }
 
 func initDatabase() {
 	var err error
-	database.DBConn, err = gorm.Open(sqlite.Open("todo.db"), &gorm.Config{})
+	database.DB, err = gorm.Open(sqlite.Open("todo.db"), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connection to the database.")
 	}
 	fmt.Println("Database connected")
+
+	database.DB.AutoMigrate(&todo.Todo{})
+	fmt.Println("Database Migrated")
 }
 
 func main() {
 	app := fiber.New()
 
 	initDatabase()
-	defer database.DBConn.Close()
 
 	setupRoutes(app)
 
--- todo/todo.go
+++ todo/todo.go
@@ -1,19 +1,54 @@
 package todo
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/rppf/go-todo-exercise/database"
+	"gorm.io/gorm"
+)
+
+type Todo struct {
+	gorm.Model
+	Title  string `json:"title"`
+	Task   string `json:"task"`
+	Author string `json:"author"`
+}
 
 func GetTodos(c *fiber.Ctx) error {
-	return c.Send([]byte("All Books"))
+	db := database.DB
+	var todos []Todo
+	db.Find(&todos)
+	return c.JSON(todos)
 }
 
 func GetTodo(c *fiber.Ctx) error {
-	return c.Send([]byte("Single Book"))
+	id := c.Params("id")
+	db := database.DB
+	var todo Todo
+	db.Find(&todo, id)
+	return c.JSON(todo)
 }
 
 func NewTodo(c *fiber.Ctx) error {
-	return c.Send([]byte("New Book"))
+	db := database.DB
+
+	newTodo := new(Todo)
+	if err := c.BodyParser(newTodo); err != nil {
+		c.Status(500).Send([]byte(err.Error()))
+	}
+
+	db.Create(&newTodo)
+	return c.JSON(newTodo)
 }
 
 func DeleteTodo(c *fiber.Ctx) error {
-	return c.Send([]byte("Deletes a book"))
+	id := c.Params("id")
+	db := database.DB
+
+	var deleteTodo Todo
+	db.First(&deleteTodo, id)
+	if deleteTodo.Title == "" {
+		return c.Status(500).Send([]byte("No book found"))
+	}
+	db.Delete(&deleteTodo)
+	return c.Send([]byte("Book Deleted"))
 }
